23d36269419a15c881f9fb4f052f02380d362010
Michael R.F <mrflowers22@uwalumni.com>
2018-10-24 13:36:16 -0400

    Initial Commit

---
+++ githubtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubtest
+//
+//  Created by Michael Flowers on 10/24/18.
+//  Copyright © 2018 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
